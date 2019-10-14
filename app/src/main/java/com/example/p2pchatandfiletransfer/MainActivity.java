4085ed6ba44d641c478540272ed06110e06ac50e
Rough22 <44208637+Rough22@users.noreply.github.com>
2019-10-15 02:17:11 +0600

    Main Activity Added

--- app/src/main/java/com/example/p2pchatandfiletransfer/MainActivity.java
+++ app/src/main/java/com/example/p2pchatandfiletransfer/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.p2pchatandfiletransfer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.text.format.Formatter;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText ip;
+    EditText port;
+    EditText portText;
+    Button connectButton;
+    TextView showIPtextId;
+    String showIPaddress;
+    String TAG = "MAIN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        portText = findViewById(R.id.myPortEditText);
+        ip = findViewById(R.id.ipEditText);
+        port = findViewById(R.id.portEditText);
+        connectButton = findViewById(R.id.connectButton);
+        showIPtextId = findViewById(R.id.showIPtextId);
+
+        WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        showIPaddress = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        showIPtextId.setText(showIPaddress);
+
+        connectButton.setOnClickListener(view -> {
+            String info = getInfo();
+            Intent intent = new Intent(MainActivity.this, chatClient.class);
+            intent.putExtra("ip&port", info);
+            startActivity(intent);
+            finish();
+        });
+
+    }
+
+    String getInfo() {
+        String info = this.ip.getText().toString()+" "+this.port.getText().toString()+" "+this.portText.getText().toString();
+        Log.i(TAG, "info => "+info);
+        return info;
     }
 }
---
+++ app/src/main/java/com/example/p2pchatandfiletransfer/chatClient.java
@@ -0,0 +1,6 @@
+package com.example.p2pchatandfiletransfer;
+
+import android.app.Activity;
+
+public class chatClient extends Activity {
+}
